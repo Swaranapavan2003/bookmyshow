e74bdcf66e1bcdb492e799fa53cfb2756377b80b
Swaranapavan2003 <148055651+Swaranapavan2003@users.noreply.github.com>
2025-01-27 15:58:42 +0530

    Update custom_filters.py

--- templatetags/custom_filters.py
+++ templatetags/custom_filters.py
@@ -1,1 +1,13 @@
+# your_app/templatetags/custom_filters.py
+from django import template
+from django.utils.timezone import localtime
+
+register = template.Library()
+
+@register.filter
+def custom_date_format(value, date_format):
+    if value:
+        local_dt = localtime(value)
+        return local_dt.strftime(date_format)
+    return value
 
